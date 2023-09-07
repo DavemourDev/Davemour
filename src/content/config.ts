7e2933d5242d207f685064c36b4fb40bb2f6b1d2
DavemourDev <dmu029018@gmail.com>
2023-09-07 23:32:11 +0200

    5 files modified, 4 added

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from 'astro:content';
+
+// See more: https://docs.astro.build/en/guides/content-collections/
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
